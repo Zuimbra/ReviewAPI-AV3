f6c452fe4d797cd4bf95c01cf0f7bf68a91919d2
Mateus Coimbra <braga.c.mateus@gmail.com>
2026-06-05 18:15:58 -0300

    feat: add core API modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { PrismaModule } from './prisma/prisma.module';
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+import { AuthModule } from './Auth/auth.module';
+import { MoviesModule } from './Movie/moviesModule';
+import { SeriesModule } from './Serie/series.module';
+import { BookModule } from './Book/bookModule';
+import { GamesModule } from './Game/gamesModule';
+import { ReviewModule } from './Review/review.module';
 
 @Module({
   imports: [
@@ -17,6 +24,13 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
 
     PrismaModule,
+    RabbitMQModule,
+    AuthModule,
+    MoviesModule,
+    SeriesModule,
+    BookModule,
+    GamesModule,
+    ReviewModule,
   ],
 })
 export class AppModule {}
