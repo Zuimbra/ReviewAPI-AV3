274626b76b93c265b801b59d83f6c937bdc2d8fd
Mateus Coimbra <braga.c.mateus@gmail.com>
2026-06-05 17:45:42 -0300

    chore: add project base structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+    }),
+
+    PrismaModule,
+  ],
 })
 export class AppModule {}
